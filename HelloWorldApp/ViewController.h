325765371ca7ea3be004488a12e6d61888ffe06a
Ferrakkem Bhuiyan <ferrakkembhuiyan@ferrakkems-MacBook-Pro.local>
2015-04-10 21:25:37 +0600

    Adding UILabels iOS app

--- HelloWorldApp/ViewController.h
+++ HelloWorldApp/ViewController.h
@@ -8,8 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    IBOutlet UILabel *helloLabel;
+    IBOutlet UITextField *nameTextField;
+}
 
+- (IBAction)sayHi:(id)sender;
 
 @end
 
--- HelloWorldApp/ViewController.m
+++ HelloWorldApp/ViewController.m
@@ -24,4 +24,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sayHi:(id)sender {
+    //Get the text of the text field
+    NSString *textFieldText = nameTextField.text;
+    //set the label to the text field
+    NSString *hiString = [[NSString alloc] initWithFormat:@"Hi, %@", textFieldText];
+    
+    helloLabel.text = hiString;
+}
 @end
